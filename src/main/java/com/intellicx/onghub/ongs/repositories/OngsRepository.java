328143e8c97fc8c4b36562e77fbda27e05848db0
RafaRamosCosta <rrcosta04@gmail.com>
2023-06-05 15:52:39 -0300

    fix: adds @Transactional.

--- src/main/java/com/intellicx/onghub/ongs/repositories/OngsRepository.java
+++ src/main/java/com/intellicx/onghub/ongs/repositories/OngsRepository.java
@@ -1,6 +1,7 @@
 package com.intellicx.onghub.ongs.repositories;
 
 import com.intellicx.onghub.ongs.models.ONGModel;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,6 +18,7 @@ public interface OngsRepository extends JpaRepository<ONGModel, UUID> {
     @Query("select e from ONGModel e where e.deletedAt=null")
     List<ONGModel> findAll();
 
+    @Transactional
     @Query("update ONGModel e set e.deletedAt=CURRENT_DATE where e.id=?1")
     @Modifying
     ONGModel softDelete(UUID id);

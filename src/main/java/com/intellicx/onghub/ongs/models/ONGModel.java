7af89b939ee86844f05aae92e189a8f9161961e2
RafaRamosCosta <rrcosta04@gmail.com>
2023-06-07 07:07:01 -0300

    feat: creates contributor relation.

--- src/main/java/com/intellicx/onghub/ongs/models/ONGModel.java
+++ src/main/java/com/intellicx/onghub/ongs/models/ONGModel.java
@@ -1,22 +1,28 @@
 package com.intellicx.onghub.ongs.models;
 
+import com.intellicx.onghub.contributors.models.ContributorModel;
 import com.intellicx.onghub.users.models.UserModel;
+import jakarta.persistence.*;
 import lombok.Data;
 
-import jakarta.persistence.*;
+import java.util.List;
 
 @Data
 @Table(name = "tb_ongs")
 @Entity
 public class ONGModel extends UserModel {
-    public ONGModel() {
-        this.setRole("ONG");
-    }
+
+    @ManyToMany(mappedBy = "ongs")
+    private List<ContributorModel> contributors;
 
     @Column(name = "pix_key")
     private String pixKey;
 
     @Column(name = "website_url")
     private String websiteUrl;
+
+    public ONGModel() {
+        this.setRole("ONG");
+    }
 
 }

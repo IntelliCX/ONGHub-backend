a64cccfcb84b5e9a130d814129f28f8c207fba4f
RafaRamosCosta <rrcosta04@gmail.com>
2023-06-07 07:04:09 -0300

    feat: creates contributor Model.

--- src/main/java/com/intellicx/onghub/contributors/models/ContributorModel.java
+++ src/main/java/com/intellicx/onghub/contributors/models/ContributorModel.java
@@ -1,10 +1,22 @@
 package com.intellicx.onghub.contributors.models;
 
+import com.intellicx.onghub.ongs.models.ONGModel;
 import com.intellicx.onghub.users.models.UserModel;
 import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.List;
+
+@Data
 @Entity
 @Table(name = "tb_contributors")
-@PrimaryKeyJoinColumn(name = "user_id")
 public class ContributorModel extends UserModel {
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "tb_ongs_contributors", joinColumns = {
+            @JoinColumn(name = "contributor_id", referencedColumnName = "id"),
+    }, inverseJoinColumns = {
+            @JoinColumn(name = "ong_id", referencedColumnName = "id")
+    })
+    private List<ONGModel> ongs;
+
 }

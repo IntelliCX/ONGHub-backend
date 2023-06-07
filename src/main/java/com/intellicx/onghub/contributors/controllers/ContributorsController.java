6a5de054436328a5f9b135500258f62ad1b38be8
RafaRamosCosta <rrcosta04@gmail.com>
2023-06-07 11:12:27 -0300

    chore: adds @Valid and @RequestBody

--- src/main/java/com/intellicx/onghub/contributors/controllers/ContributorsController.java
+++ src/main/java/com/intellicx/onghub/contributors/controllers/ContributorsController.java
@@ -13,9 +13,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "Contributors", description = "Manages Contributors")
@@ -44,7 +46,7 @@ public class ContributorsController {
     @ApiResponse(responseCode = "201", description = "Contributor created", content = @Content(schema = @Schema(implementation = ContributorModel.class)))
     @ApiResponse(responseCode = "409", description = "Contributor's email already registred", content = @Content(schema = @Schema(example = "{ \"data\": \"This email is already registred\" }")))
     @PostMapping("/create")
-    ResponseEntity<Object> createContributor(CreateOrUpdateContributorDto createOrUpdateContributorDto){
+    ResponseEntity<Object> createContributor(@Valid @RequestBody CreateOrUpdateContributorDto createOrUpdateContributorDto){
         GenericResponse response = this.createContributorService.execute(createOrUpdateContributorDto);
 
         return ResponseEntity.status(response.status()).body(response.data());
